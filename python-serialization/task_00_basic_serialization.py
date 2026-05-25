989190361af54c8238e3d95b65b87b21b1d5a2e2
David Lengellé <david.lengelle.1.10@gmail.com>
2026-05-25 15:56:31 +0200

    First commit

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Serialize a dictionary to a JSON file and deserialize it back"""
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """Serialize a dictionary and save it to a JSON file"""
+
+    with open(filename, mode="w") as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """Load a JSON file and deserialize it into a dictionary"""
+
+    with open(filename, mode="r") as f:
+        data = json.load(f)
+    return data
