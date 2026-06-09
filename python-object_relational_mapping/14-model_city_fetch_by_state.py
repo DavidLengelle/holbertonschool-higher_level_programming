3c24dc9170ded9b2b608332bab5a857dab93e36d
David Lengellé <david.lengelle.1.10@gmail.com>
2026-06-09 20:45:34 +0200

    Add script to print all City objects with their state from hbtn_0e_14_usa

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Prints all City objects with their state from hbtn_0e_14_usa"""
+
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    results = session.query(State, City).filter(
+        City.state_id == State.id).order_by(City.id).all()
+    for state, city in results:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
