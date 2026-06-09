39577e72856737136f8d2b7d6c678ef871a88706
David Lengellé <david.lengelle.1.10@gmail.com>
2026-06-09 20:04:57 +0200

    Add script to print the id of a State object based on its name

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Prints the id of the State whose name is passed as argument"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
+    session.close()
