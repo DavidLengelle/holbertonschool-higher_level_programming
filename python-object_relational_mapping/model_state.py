1416a72e7432cbad097c762401c7293b347ca006
David Lengellé <david.lengelle.1.10@gmail.com>
2026-06-09 19:11:12 +0200

    Add State model definition mapped to the states table

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines the State model mapped to the states table"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Represents a state stored in the states table"""
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                nullable=False)
+    name = Column(String(128), nullable=False)
